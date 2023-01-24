d32e3b242f04e8a361e79d8a6d6abf3d0f25b517
FudgyWudgy27 <Zer042711@gmail.com>
2023-01-24 12:07:04 -0500

    AlgorithmCheck

---
+++ Homework/AnimationAndConditionalOperators/AlgorithmCheck.js
@@ -0,0 +1,19 @@
+/*
+My plan for this assignment is to create a main HTML page that will contain links to 3 other HTML pages. 
+Each HTML page will consist of a different visual. 
+The visuals will be "Ever larger, until not," "Ball Bounce," and "World Wrap." 
+
+For the "Ever larger, until not," I will need to use an if() statement to determine when the circle reaches 200 pixels so that I can revert it back down to 1 pixel.
+Depending on how the code works, I might need to add a for() statement to loop the process.
+
+For the "Ball Bounce," I will need to use if() statements to check if the radius of the ball is touching the edge of the canvas. 
+If the radius is touching it, then I need the ball to go a different direction.
+
+For the "World Wrap," I will need to use an if() statement to determine if the radius of the ball has started to go outside of the right side of the canvas edge.
+When it detects even a pixel of the radius, then I want it to create a new ball on the left side of the cavas edge.
+Once the first ball gets off the screen, I want it to be deleted and for the second ball to have fully appeared on the left side.
+My goal is to make it look like the ball is reappearing on the left side while it disappears on the right.
+I might need a for() loop statement to do this.
+
+If I have more time after these three are done, I will attempt to do the other two visuals also.
+*/
