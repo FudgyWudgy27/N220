01130fdd276b71bee2a02d5d087cd5b9c420bb1c
FudgyWudgy27 <Zer042711@gmail.com>
2023-01-26 15:39:51 -0500

    Update app.js

--- Homework/AnimationAndConditionalOperators/counter/app.js
+++ Homework/AnimationAndConditionalOperators/counter/app.js
@@ -1,3 +1,4 @@
+//Variable for the number
 let number = 0;
 
 function setup() {
@@ -6,7 +7,9 @@ function setup() {
 
 function draw() {
 
+    //Adds 1 to the number variable every frame
     number += 1;
 
+    //Prints the value of the number variable in the console
     print(number);
 }
