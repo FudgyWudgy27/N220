11d7550ca47c2f6ce9ea97a399ece9473d28997f
FudgyWudgy27 <Zer042711@gmail.com>
2023-01-30 17:42:55 -0500

    Create AlgorithmCheck.js

---
+++ Homework/FunctionsExercises1/AlgorithmCheck.js
@@ -0,0 +1,15 @@
+/*
+For the Drawing Drawer I'm not entirely sure what it is asking for me to do. 
+I believe that it is asking for when a key is pressed, a part of the drawing appears. 
+Once all parts appear, there should be atleast three copies of the drawing. 
+Each copy should follow the mouse.
+My plan is for there to be 4 copies and each one being a certain number of pixels away from the mouse at all time.\
+I'll have to use the object function to group the shapes together.
+Use a let object1 = mouseX/Y +/- ###.
+
+
+For the Red Remover I believe that it is asking for me to write lines of code similar to what is given in the assignment.
+This code will need to start with a predermined color and be able to remove an aspect of the rgb color code.
+
+For the Polar Points I believe that it wants me to pull from the results of a sin/cosine function and to draw circles along it, without any circle overlapping.
+*/
