c647a329cee76c30d419738e454ea053273f4e3a
FudgyWudgy27 <Zer042711@gmail.com>
2023-02-06 17:57:32 -0500

    Create AlgorithmCheck.js

---
+++ Homework/ObjectExercises/AlgorithmCheck.js
@@ -0,0 +1,11 @@
+/*
+For the first task, I believe that it is asking for me to make an object that contains all of the numbers needed to make a shape with a color it.
+I then need to make a shape using fill and drawing commands to make this shape appear while only using the variables from the object and litle to no numbers.
+I then need to make a funciton that also creates the shape.
+
+For the second task I'll need to make objects for two tall rectangles that are the same size.
+I then need to use those object variables to draw the the rectangles beside each other.
+Then I need to use an if() statement and have one rectangle move up and the other move down depending on what key is pressed.
+
+For the final assignment, I need to revamp my old ball bounce assignment using objects instead of hard numbers in the creation process.
+*/
