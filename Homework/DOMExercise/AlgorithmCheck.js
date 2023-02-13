8f441fc25be8cae9a28e6aa63b75433aeca7f84d
FudgyWudgy27 <Zer042711@gmail.com>
2023-02-13 15:15:08 -0500

    Create AlgorithmCheck.js

---
+++ Homework/DOMExercise/AlgorithmCheck.js
@@ -0,0 +1,23 @@
+/*
+First task:
+Will need to make a 100 px by 100 px box that has a green background.
+When the box is clicked, they box increases by 10% in both directions.
+Will need variabels for width and height.
+Will need to add:
+if (mouseIsPressed){
+    width/height * 1.1;
+}
+
+Second Task:
+Have a div on the page, that when clicked will display the text "mc".
+Need a way to detect when it has been clicked three times and display "divdivdiv".
+
+Third Task:
+Make a blue box that is 100 px by 100 px.
+when moused over, the box is now black, when mouse leaves box, goes back to blue.
+Will need onmouseover and onmouseout in code.
+Experiemnt with:
+if(onmouseover/onmouseout){
+    fill(black/blue);
+}
+*/
