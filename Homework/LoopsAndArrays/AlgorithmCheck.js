df7216673222561a19699d1d3b67da652eada31f
FudgyWudgy27 <Zer042711@gmail.com>
2023-02-20 15:15:27 -0500

    Create AlgorithmCheck.js

---
+++ Homework/LoopsAndArrays/AlgorithmCheck.js
@@ -0,0 +1,19 @@
+/*
+For the Loop, but Big task I will need a variable that will start out at 1000.
+I will need to write a loop that displays the number of the variable into the div line. 
+When the loop starts back over, it will need to add another 1000 to the variable.
+
+For the Beep Bop task, I will need a variable that starts at either 0 or 1.
+I will need a loop that will write what iteration the loop is on and increase the variable by one.
+When the variable can be divided by 3 the text will instead write "beep"
+When the variable can be divided by 5 the text will instead write "bop"
+When the variable can be divided by 3 and 5 the text will instead write "beepbop"
+
+For the Runner Up task, I will need an array that lists the three best times of the day.
+[00:00, 03:00, 21:00]
+Will need to write code that displays the the second and third best times on two different divs.
+
+For the Favorry task, I will need an array that lists my 5 favorite things.
+Will need a loop that displays one item from that array with the text "is one of my favorite things."
+Each item will need it's own div line.
+*/
