4d9e0e011ca3c678e9032ec80251f82486fb2a8f
FudgyWudgy27 <Zer042711@gmail.com>
2023-03-27 17:54:40 -0400

    Create AlgorithmCheck.js

---
+++ Homework/FunctionsExercises2/AlgorithmCheck.js
@@ -0,0 +1,22 @@
+/*
+The first task that I will do will be the "Is Divisible by Seven" task.
+In this task I will need to make an input that only accepts numbers, a button, and a div that will be used later on.
+I'll then need a function that when ran (onclick for the button) will check if the another function is equal to a whole number.
+The first function will divide the input value by 7.
+The second function will display true or false in the div depending on whether or not the first funcion is divisble by 7.
+function secondFunction(){
+    Number.isInteger(firstFunction)
+}
+
+
+The second task that I will do will be the "Make Random" task.
+In this task I will need a button and a div that will be used later on.
+I will also need a function that generates a random whole number from 0-10 and another function that will call the generator function.
+When the button is button is clicked, the second function will run, which in turn will call the other function and generate a random number.
+The second function will then display that random number into the div line.
+
+The third task that I will do will be the "Show Info" taks.
+In this I will need three buttons and one dive line that will be used later.
+I will also need an array containing three different strings.
+I will need a function that when one of the buttons is clicked, it will have the value in the array that is associated with it displayed into the div line.
+*/
