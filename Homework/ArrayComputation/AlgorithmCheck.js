ebc70dac22da4a83d99edade701f435f184893e5
FudgyWudgy27 <Zer042711@gmail.com>
2023-04-03 15:12:42 -0400

    Create AlgorithmCheck.js

---
+++ Homework/ArrayComputation/AlgorithmCheck.js
@@ -0,0 +1,15 @@
+/*
+The three tasks that I will do for this Homework are Make Divs, Check Win, and Check Win Function.
+
+For the Make Divs task, I will need to copy over the array then make a loop that turns the contents of the array into a div line.
+let objects = [blah]
+for(i=0; i==objects.length;i++){
+    creating the div
+}
+
+For the Check Win ask, I will need write lines of code that will take the value form the input field and turn it into an array. 
+I will then need a loop that checks if there are three "1" in a row.
+If so, it will display "Winner" if not then it will display "Not Winner"
+
+For the Check Win Function, I wll need to do the same thing as the last task, but in the form of a funciton.
+*/
