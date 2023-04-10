b91aba06ecd90d3c581d4bc8b1f6c2b3417a84ad
FudgyWudgy27 <Zer042711@gmail.com>
2023-04-10 15:09:34 -0400

    Create AlgorithmCheck.js

---
+++ Homework/EventsExercises/AlgorithmCheck.js
@@ -0,0 +1,17 @@
+/*
+For the Color Changer task, I will need to create three gray divs that are 200px by 200px and have 5px inbetween each other.
+I will need on event listner that will be detecting when a div is clicked.
+Depending on which div is clicked, it will change a different color. Red, green, blue respectfully.
+
+For the Color Mixer task, I will need to create two divs and 9 buttons.
+One div will start out black, but later display a color depending on what buttons are clicked.
+The second div will display the current rgb values.
+The 9 buttons will then be divided into three rows (Red, green, blue).
+The three buttons in each row will show +1, +5, and +10.
+Based on what button is clicked, the rgb values will change, which will end up changing both divs.
+
+For the Flash Cards task, I will need three divs with a button in each.
+Each button will hold a question.
+When the button is clicked, the answer to the question will be displayed beside it in the div line.
+Can only use one function.
+*/
